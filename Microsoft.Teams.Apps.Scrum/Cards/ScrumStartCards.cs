c2bcddd7744ee0a7397d65091d5f8746b7291a5d
Viknesh Mohan <vimohan@microsoft.com>
2020-11-02 19:36:34 +0530

    test

--- Microsoft.Teams.Apps.Scrum/Cards/ScrumStartCards.cs
+++ Microsoft.Teams.Apps.Scrum/Cards/ScrumStartCards.cs
@@ -102,7 +102,7 @@ namespace Microsoft.Teams.Apps.Scrum.Cards
                         {
                             Size = AdaptiveTextSize.Medium,
                             Wrap = true,
-                            Text = cardMessage + " Debug ",
+                            Text = cardMessage + " Debug 2",
                         },
                     },
             };
