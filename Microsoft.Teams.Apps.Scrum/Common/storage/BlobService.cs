5dbca6e41f5b71511c35cf1126ab99250772606a
Viknesh Mohan <vimohan@microsoft.com>
2020-11-04 20:49:46 +0530

    adding report functionality

--- Microsoft.Teams.Apps.Scrum/Common/storage/BlobService.cs
+++ Microsoft.Teams.Apps.Scrum/Common/storage/BlobService.cs
@@ -19,6 +19,7 @@ namespace Microsoft.Teams.Apps.Scrum.storage
         private readonly Lazy<Task> initializeTask;
         private TelemetryClient telemetryClient;
         private BlobContainerClient reportContainerClient;
+        private static readonly string reportContainerName = "reports";
 
         // https://27xnwgjign2tq.blob.core.windows.net/test?sv=2019-12-12&st=2020-11-04T13%3A41%3A24Z&se=2020-11-04T14%3A41%3A24Z&sr=b&sp=r&sig=61ZOV6TVjDlfPfA9X6AcNvpgAFkvnraZNPEzu0DZv2s%3D&comp=list&restype=container
 
@@ -32,7 +33,8 @@ namespace Microsoft.Teams.Apps.Scrum.storage
             try
             {
                 BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
-                this.reportContainerClient = blobServiceClient.GetBlobContainerClient("test");
+                this.reportContainerClient = blobServiceClient.GetBlobContainerClient(reportContainerName);
+                await this.reportContainerClient.CreateIfNotExistsAsync();                
             }
             catch (Exception ex)
             {
@@ -43,17 +45,33 @@ namespace Microsoft.Teams.Apps.Scrum.storage
 
         public async Task<string> SaveReportAndGetUri(List<ScrumDetailsEntity> scrumUpdates, string conversationId, DateTimeOffset startTime, DateTimeOffset endTime)
         {
-            await this.EnsureInitializedAsync();
-            var csv = new StringBuilder();
-            csv.AppendLine("Date,Name,Yesterday,Today,Blockers");
-            foreach (ScrumDetailsEntity entity in scrumUpdates)
+            var blobUri = "";
+            var uuid = System.Guid.NewGuid();
+            var tmpPath = Path.Combine(Environment.CurrentDirectory, uuid + ".csv");
+            try
             {
-                csv.AppendLine(entity.Timestamp.ToString() + "," + entity.Name + "," +
-                    entity.Yesterday + "," + entity.Today + "," + entity.Blockers);
+                await this.EnsureInitializedAsync();
+                var csv = new StringBuilder();
+                csv.AppendLine("Date,Name,Yesterday,Today,Blockers");
+                foreach (ScrumDetailsEntity entity in scrumUpdates)
+                {
+                    csv.AppendLine(entity.Timestamp.ToString() + "," + entity.Name + "," +
+                        entity.Yesterday + "," + entity.Today + "," + entity.Blockers);
+                }
+                var blobName = string.Format("/{0}/{1}/{2}/report.csv", conversationId, startTime.Date.ToString(), endTime.Date.ToString());
+                await File.WriteAllTextAsync(tmpPath, csv.ToString());
+                var blobClient = this.reportContainerClient.GetBlobClient(blobName);
+                using FileStream uploadFileStream = File.OpenRead(tmpPath);
+                await blobClient.UploadAsync(uploadFileStream, true);
+                uploadFileStream.Close();
+                blobUri = this.GetBlobSasUri(this.reportContainerClient, blobName, null);
             }
-            var imagePath = Path.Combine(Environment.CurrentDirectory, "architecture-resize.png");
-            
-            return this.GetBlobSasUri(this.reportContainerClient, "sample_test.txt", null);
+            finally {
+                if (File.Exists(tmpPath)) {
+                    File.Delete(tmpPath);
+                }
+            }
+            return blobUri;
         }
 
         private string GetBlobSasUri(BlobContainerClient container, string blobName, string storedPolicyName = null)
@@ -64,7 +82,7 @@ namespace Microsoft.Teams.Apps.Scrum.storage
             {
                 BlobContainerName = "test",
                 BlobName = blobName,
-                Resource = "c",
+                Resource = "b",
             };
 
             if (storedPolicyName == null)
