1cfae5e72de852b81becd0b2c52773c5556c5b0d
Mahendra Singh <mahsingh@microsoft.com>
2020-11-05 20:07:57 +0530

    Added ARM template

--- Microsoft.Teams.Apps.Scrum/Properties/Resources.Designer.cs
+++ Microsoft.Teams.Apps.Scrum/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Microsoft.Teams.Apps.Scrum.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -223,7 +223,7 @@ namespace Microsoft.Teams.Apps.Scrum.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Nice Work! This scrum is complete..
+        ///   Looks up a localized string similar to Thank you! This scrum is complete..
         /// </summary>
         internal static string ScrumCompleteText {
             get {
